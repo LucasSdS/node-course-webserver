ccc1d70943e5ee6a4fc92bfb2e1f62937f7bd4be
lucas <lucassantos10@gmail.com>
2019-02-01 14:08:14 -0200

    Initial Commit

---
+++ server.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+const app = express();
+
+hbs.registerPartials(`${__dirname}/views/partials`);
+app.set('view engine', 'hbs');
+app.use(express.static(`${__dirname}/public`));
+
+app.use((req, res, next) => {
+    const now = new Date().toString();
+    const log = `${now}: ${req.method} ${req.url}`;
+
+    fs.appendFile('server-log', log + '\n', err =>{
+        if(err){
+            console.log('error');
+        }
+    });
+
+    next();
+});
+
+app.use((req, res, next) => {
+    if(maintenance){
+        res.render('maintenance.hbs', {
+            pageTitle: 'Maintenance',
+            welcome: 'COME BACK SOON'
+        });
+    }else{
+        next();
+    }
+});
+
+hbs.registerHelper('getCurrentYear', () => new Date().getFullYear());
+hbs.registerHelper('screamIt', text => text.toUpperCase());
+
+app.get('/', (req, res) => {
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        welcome: 'Site'
+    });
+
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs',{
+        pageTitle: 'About Page'
+    });
+});
+
+app.get('/bad', (req, res) => {
+    res.send({
+        errorMessage: 'ERROR! ERROR! ERROR!'
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Server is up in port 3000');
+});
+
+const maintenance = false;
